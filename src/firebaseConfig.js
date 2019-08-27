c263f6214cac7e5cbdb8d46ee0412063beedeca5
Jamel Eddine Mejri <edd@wevioo.com>
2019-08-27 15:39:28 +0100

    firebase config file structure

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
 const firebase = require('firebase')
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAuXv5h0nIViOEKQ-89zgA5QvElcezm-XU",
-    authDomain: "x-cite-28d9b.firebaseapp.com",
-    databaseURL: "https://x-cite-28d9b.firebaseio.com",
-    projectId: "x-cite-28d9b",
+    apiKey: "****",
+    authDomain: "****",
+    databaseURL: "***",
+    projectId: "***",
     storageBucket: "",
-    messagingSenderId: "958366267663",
-    appId: "1:958366267663:web:d64f9a6baa65af5f"
+    messagingSenderId: "****",
+    appId: "****"
 };
 firebase.initializeApp(firebaseConfig);
 const providers = new firebase.auth.GoogleAuthProvider();
